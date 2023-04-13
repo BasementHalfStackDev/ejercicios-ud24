32c9fbf1f657b0e293c255a3eada4b45e32a29d6
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-13 10:39:13 +0200

    First commit

---
+++ JPallas_TA24/src/main/java/JPallas/TA24/JPallasTa24Application.java
@@ -0,0 +1,13 @@
+package JPallas.TA24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPallasTa24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPallasTa24Application.class, args);
+	}
+
+}
---
+++ JPallas_TA24/src/test/java/JPallas/TA24/JPallasTa24ApplicationTests.java
@@ -0,0 +1,13 @@
+package JPallas.TA24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPallasTa24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
